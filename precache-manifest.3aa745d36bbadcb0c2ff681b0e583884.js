2b80e6a7dca605d3b973d25412da5cb2ebbb889b
PatrickRamsayGilbert <i_be_pretty@yahoo.com>
2020-11-13 16:53:41 -0500

    Updates

---
+++ precache-manifest.3aa745d36bbadcb0c2ff681b0e583884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f2fa56277e76bd8415bfcc6453f56b",
+    "url": "/stake-dai/index.html"
+  },
+  {
+    "revision": "57a5373310d71e19f256",
+    "url": "/stake-dai/static/css/2.25d2a862.chunk.css"
+  },
+  {
+    "revision": "e8d1669ee10df01aa03a",
+    "url": "/stake-dai/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "57a5373310d71e19f256",
+    "url": "/stake-dai/static/js/2.ebc77ced.chunk.js"
+  },
+  {
+    "revision": "e8d1669ee10df01aa03a",
+    "url": "/stake-dai/static/js/main.6d3e7bec.chunk.js"
+  },
+  {
+    "revision": "5d18e6a7c83bc361833e",
+    "url": "/stake-dai/static/js/runtime-main.ffb703cf.js"
+  }
+]);
